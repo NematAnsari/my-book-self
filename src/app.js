4350a05af3bf66320f62240a185fcaa84e8f4143
Nemat Ansari <nematansari0@gmail.com>
2024-10-02 03:18:08 +0530

    create basic controller and router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,32 @@
 import connectDB from "./db/connectDB.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 dotenv.config();
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running on this port :${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+  });
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/users", userRouter);
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const registerUser = async (req, res) => {
+  res.status(200).json({ message: "Okg" });
+};
+
+export { registerUser };
